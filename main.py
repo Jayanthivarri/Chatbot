003c98b37c004018df9495042bc03cc6434e56ea
jayanthivarri <jayanthivarri@gmail.com>
2026-07-22 11:23:26 +0530

    Implemented terminal chatbot with summarization

--- main.py
+++ main.py
@@ -2,9 +2,11 @@ from llm import llm_call
 from colorama import Fore, Style, init
 
 init(autoreset=True)
+
 messages = []
+
 print(Fore.CYAN + "=" * 40)
-print(Fore.GREEN + " AI Chatbot")
+print(Fore.GREEN + "       AI Chatbot    ")
 print(Fore.CYAN + "=" * 40)
 
 while True:
@@ -13,15 +15,36 @@ while True:
     if user_input.lower() in ["stop", "exit", "quit"]:
         print(Fore.GREEN + "\nBot : " + Style.RESET_ALL + "Goodbye!")
         break
-    print(Fore.BLUE + "\nThinking...")
-    response = llm_call(user_input, messages)
 
-    print(Fore.GREEN + "\nBot : " + Style.RESET_ALL + response)
+    print(Fore.BLUE + "\nThinking...")
+
+    response = llm_call(user_input, messages)
+    print(Fore.LIGHTMAGENTA_EX + "\nBot : " + response + Style.RESET_ALL)
 
     messages.append({
         "q": user_input,
         "a": response
     })
 
-   
-    #print(messages)
+    
+    if len(messages) % 5 == 0:
+
+        conversation = ""
+
+        for chat in messages[-5:]:
+            conversation += f"""
+                User: {chat['q']}
+                Assistant: {chat['a']}
+                """
+
+        summary_prompt = f"""
+            Summarize the following conversation in 4-5 lines.
+
+            {conversation}
+            """
+
+        print(Fore.MAGENTA + "\nGenerating Summary...\n")
+
+        summary = llm_call(summary_prompt, [])
+
+        print(Fore.CYAN + "Summary : " + Style.RESET_ALL + summary)
---
+++ readme.md
@@ -0,0 +1,47 @@
+# AI Chatbot
+
+A simple terminal-based AI chatbot built using Python and the OpenRouter API. The chatbot maintains session memory during runtime and uses a custom system prompt for generating responses.
+
+## Features
+
+- Terminal-based chatbot
+- Session memory (current session only)
+- Custom system prompt
+- User profile support using JSON
+- Powered by OpenRouter API
+- Colored terminal output using Colorama
+
+## Project Structure
+
+```
+chatbot/
+│── main.py
+│── llm.py
+│── prompt.py
+│── compressor.py
+│── profile.example.json
+│── requirements.txt
+│── .env.example
+│── README.md
+```
+
+## Technologies Used
+
+- Python
+- OpenRouter API
+- OpenAI Python SDK
+- Colorama
+- python-dotenv
+
+
+## Future Improvements
+
+- Conversation summarization
+- Web search
+- RAG (Retrieval-Augmented Generation)
+- Tool calling
+- Long-term memory
+
+## License
+
+This project is for learning and educational purposes.
